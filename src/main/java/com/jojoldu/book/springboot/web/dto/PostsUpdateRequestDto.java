17f3d7980bab2bb367bdb90507e48348e7960a81
Dongha <kdongha1597@gmail.com>
2020-03-17 16:33:06 +0900

    initial project

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void postsLoadTest() {
+        //given
+        String title = "title";
+        String content = "content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntityTest() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
+}
